a9685202b7dedf2b7479b826142ed8f31f34201e
Nurillo <nabdi1320@gmail.com>
2022-01-31 21:34:04 -0600

    Initial commit

---
+++ app/src/main/java/com/example/helloworld2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.helloworld2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) findViewById(R.id.text)).setTextColor(getResources().getColor(R.color.white));
+            }
+        });
+    }
+}
